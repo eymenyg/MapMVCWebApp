7a9bc8dc6cc7af4ae7c7c5590ea21cacc68aa37f
eymenyg <42818471+eymenyg@users.noreply.github.com>
2022-06-14 16:35:22 +0300

    LocationModel: instantiate Title as empty

--- Models/LocationModel.cs
+++ Models/LocationModel.cs
@@ -3,7 +3,7 @@
     public class LocationModel
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string Title { get; set; } = string.Empty;
         public double Latitude { get; set; }
         public double Longitude { get; set; }
 
