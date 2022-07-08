dd3ddff6d98c90c2c645ffe03d48caf4a0222a0c
eymenyg <42818471+eymenyg@users.noreply.github.com>
2022-07-08 14:03:11 +0300

    set required model attributes

--- Models/LocationModel.cs
+++ Models/LocationModel.cs
@@ -1,10 +1,15 @@
-﻿namespace MapMVCWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MapMVCWebApp.Models
 {
     public class LocationModel
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; } = string.Empty;
+        [Required]
         public double Latitude { get; set; }
+        [Required]
         public double Longitude { get; set; }
 
         public LocationModel()
